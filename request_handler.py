e0ebe27015e0c7e6b5c2510057b1cbc1f0b9d32a
Lance Buckley <erchancy@gmail.com>
2023-07-13 15:51:51 -0500

    DRY refactoring

--- request_handler.py
+++ request_handler.py
@@ -1,61 +1,44 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_metals, get_single_metal,\
-    get_all_sizes, get_single_size,\
-    get_all_styles, get_single_style,\
-    get_all_orders, get_single_order, create_order, delete_order, update_order
+from repository import retrieve, all, create, update, delete
+
+method_mapper = {
+    "metals": {"single": retrieve, "all": all, "create": create, "update": update, "delete": delete},
+    "sizes": {"single": retrieve, "all": all, "create": create, "update": update, "delete": delete},
+    "styles": {"single": retrieve, "all": all, "create": create, "update": update, "delete": delete},
+    "orders": {"single": retrieve, "all": all, "create": create, "update": update, "delete": delete}
+}
 
 
 class HandleRequests(BaseHTTPRequestHandler):
-    """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
-    """
+    """Controls the functionality of any GET, PUT, POST, DELETE requests to the server"""
 
-    def do_GET(self):
-        """Handles GET requests to the server """
+    def get_all_or_single(self, resource, id):
+        if id is not None:
+            response = method_mapper[resource]["single"](resource, id)
 
-        # Set the response code to 'Ok'
-        self._set_headers(200)
-        response = {}  # Default response
-
-        # Parse the URL and capture the tuple that is returned
-        (resource, id) = self.parse_url(self.path)
-
-        if resource == "metals":
-            if id is not None:
-                response = get_single_metal(id)
-
+            if response is not None:
+                self._set_headers(200)
             else:
-                response = get_all_metals()
-
-        elif resource == "sizes":
-            if id is not None:
-                response = get_single_size(id)
-
-            else:
-                response = get_all_sizes()
-
-        elif resource == "styles":
-            if id is not None:
-                response = get_single_style(id)
-
-            else:
-                response = get_all_styles()
-
-        elif resource == "orders":
-            if id is not None:
-                response = get_single_order(id)
-
-            else:
-                response = get_all_orders()
-
+                self._set_headers(404)
+                response = ''
         else:
-            response = []
+            self._set_headers(200)
+            response = method_mapper[resource]["all"](resource)
 
+        return response
+
+    # Here's a method on the class that overrides the parent's method.
+    # It handles any GET request.
+    def do_GET(self):
+        response = None
+        (resource, id) = self.parse_url(self.path)
+        response = self.get_all_or_single(resource, id)
         self.wfile.write(json.dumps(response).encode())
 
+    # Here's a method on the class that overrides the parent's method.
+    # It handles any POST request.
     def do_POST(self):
-        """Handles POST requests to the server """
-        self._set_headers(201)
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
 
@@ -65,13 +48,10 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Parse the URL
         (resource, id) = self.parse_url(self.path)
 
-        # Initialize new order
-        new_order = None
+        response = method_mapper[resource]["create"](resource, post_body)
 
-        if resource == "orders":
-            new_order = create_order(post_body)
-            response = new_order
-
+        self._set_headers(201)
+        # Encode the new animal and send in response
         self.wfile.write(json.dumps(response).encode())
 
     # A method that handles any PUT request.
@@ -84,14 +64,13 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Parse the URL
         (resource, id) = self.parse_url(self.path)
 
-        # Delete a single order from the list
-        if resource == "orders":
-            update_order(id, post_body)
+        method_mapper[resource]["update"](id, resource, post_body)
 
-        # Encode the new order and send in response
+        # Encode the new animal and send in response
         self.wfile.write("".encode())
 
     def _set_headers(self, status):
+        # Notice this Docstring also includes information about the arguments passed to the function
         """Sets the status code, Content-Type and Access-Control-Allow-Origin
         headers on the response
 
@@ -103,6 +82,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
 
+    # Another method! This supports requests with the OPTIONS verb.
     def do_OPTIONS(self):
         """Sets the options headers
         """
@@ -114,23 +94,10 @@ class HandleRequests(BaseHTTPRequestHandler):
                          'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
-    def do_DELETE(self):
-        # Set a 204 response code
-        self._set_headers(204)
-
-        # Parse the URL
-        (resource, id) = self.parse_url(self.path)
-
-        # Delete a single order from the list
-        if resource == "orders":
-            delete_order(id)
-
-        self.wfile.write("".encode())
-
     def parse_url(self, path):
         # Just like splitting a string in JavaScript. If the
-        # path is "/orders/1", the resulting list will
-        # have "" at index 0, "orders" at index 1, and "1"
+        # path is "/animals/1", the resulting list will
+        # have "" at index 0, "animals" at index 1, and "1"
         # at index 2.
         path_params = path.split("/")
         resource = path_params[1]
@@ -142,13 +109,25 @@ class HandleRequests(BaseHTTPRequestHandler):
             # This is the new parseInt()
             id = int(path_params[2])
         except IndexError:
-            pass  # No route parameter exists: /orders
+            pass  # No route parameter exists: /animals
         except ValueError:
-            pass  # Request had trailing slash: /orders/
+            pass  # Request had trailing slash: /animals/
 
         return (resource, id)  # This is a tuple
 
+    def do_DELETE(self):
 
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        method_mapper[resource]["delete"](id, resource)
+
+        # Set a 204 response code
+        self._set_headers(204)
+        self.wfile.write("".encode())
+
+
+# This function is not inside the class. It is the starting
 # point of this application.
 def main():
     """Starts the server on port 8088 using the HandleRequests class
